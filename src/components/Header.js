97f040098590b1faf6033a7b6e8c47bec747a0ab
Rasheeda Abbas <rasheedaabbas@gmail.com>
2021-03-24 23:28:08 +0500

    button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-const Header = (props) => {
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("hello");
+  };
+
   return (
-    <header>
-      <h1>{props.title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button text="Add" color="green" onClick={onClick} />
     </header>
   );
 };
 
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const headingStyle = {
+//   color: "white",
+//   backgroundColor: "darkslateblue",
+// };
 export default Header;
